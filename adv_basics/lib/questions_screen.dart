9e914e8f9ab31648c9c54b61bb838e6e434b3af6
galiyev <kairat.galiyev@gmail.com>
2024-05-04 13:54:35 +0500

    Add / Lesson 67 / Questions Data

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,7 +1,7 @@
 import 'package:adv_basics/answer_button.dart';
-import 'package:adv_basics/models/quiz_question.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -13,9 +13,18 @@ class QuestionsScreen extends StatefulWidget{
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen>{
+
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(){
+    setState((){
+        currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -27,14 +36,16 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
           children: [
             Text(
                 currentQuestion.text,
-                style: const TextStyle(
-                  color: Colors.white
+                style: GoogleFonts.lato(
+                  color: Color.fromARGB(255, 201, 153, 251),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold
                 ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30,),
-            ...currentQuestion.answers.map((answer){
-              return AnswerButton(answerText: answer, onTap: (){});
+            ...currentQuestion.getShuffledAnswers().map((answer){
+              return AnswerButton(answerText: answer, onTap:answerQuestion);
             })
         ],),
       ),
--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -17,11 +18,13 @@ class StartScreen extends StatelessWidget {
                 color: Color.fromARGB(150, 255, 255, 255),
               ),
               const SizedBox(height: 80,),
-              const Text('Learn Flutter the fun way!', 
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24
-                          ),),
+              Text(
+                  'Learn Flutter the fun way!',
+                  style: GoogleFonts.lato(
+                    fontSize:24,
+                    color: Color.fromARGB(255, 237, 223, 252),
+                  ),
+              ),
              const SizedBox(height: 20,),
              OutlinedButton.icon(
                  onPressed:startQuiz,
