85a8ab7da625046b2110344b839222d6d3d8d73e
galiyev <kairat.galiyev@gmail.com>
2024-05-04 12:30:17 +0500

    Add / Lesson 67 / Questions Data

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,4 +1,7 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/models/quiz_question.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -12,10 +15,31 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionsScreenState extends State<QuestionsScreen>{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const Text('Hallo!');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+                currentQuestion.text,
+                style: const TextStyle(
+                  color: Colors.white
+                ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30,),
+            ...currentQuestion.answers.map((answer){
+              return AnswerButton(answerText: answer, onTap: (){});
+            })
+        ],),
+      ),
+    );
   }
-  
 }
 
 
