e9a6f1afda0a0db5b28539aa0995263e6c45ea50
galiyev <kairat.galiyev@gmail.com>
2024-05-10 23:53:34 +0500

    Lesson / 136

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/chart/chart_bar.dart';
 import 'package:expense_tracker/widgets/expense_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../models/expense.dart';
@@ -66,6 +67,11 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+
+    final width = MediaQuery.of(context).size.width;
+    print(MediaQuery.of(context).size.height);
+
+
     Widget mainContent = Center(child: Text("No expenses found. Start adding some"),);
 
     if (_registeredExpenses.isNotEmpty){
@@ -82,7 +88,7 @@ class _ExpensesState extends State<Expenses> {
                 icon: const Icon(Icons.add))
          ],
        ),
-       body: Column(
+       body: width<600?Column(
           children: [
               //  Toolbar with add button
               Chart(expenses: _registeredExpenses,),
@@ -90,7 +96,14 @@ class _ExpensesState extends State<Expenses> {
                 child: mainContent,
               )
           ],
-       ),
+       ):Row(children: [
+             Expanded(
+                 child: Chart(expenses: _registeredExpenses,)
+             ),
+             Expanded(
+               child: mainContent,
+             )
+       ],),
     );
   }
 }
