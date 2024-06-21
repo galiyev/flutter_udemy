7160ff073cc556c298a395e80f2fef2075d1cf29
galiyev <kairat.galiyev@gmail.com>
2024-06-21 09:01:38 +0500

    Add / tree_app application

---
+++ Part7/tree_app/lib/demo_buttons.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class DemoButtons extends StatefulWidget {
+  const DemoButtons({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+      return _DemoButtonsState();  }
+
+}
+
+class _DemoButtonsState extends State<DemoButtons>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
